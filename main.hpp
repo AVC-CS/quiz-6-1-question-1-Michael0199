fc8c4fe677879bf8af98cf64d6ef590c0bc03139
Michael Hodges <156959963+Michael0199@users.noreply.github.com>
2024-03-13 23:20:26 +0000

    vomis

--- main.hpp
+++ main.hpp
@@ -1,8 +1,53 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int&,int&);
+int getNextPrime(int);
+int getPrevPrime(int);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int& a,int& b){
+cout<< "Enter two integer values: ";
+int d=2;
+for (int i=0; i<d; i++){
+cin>>a>>b;
+if (a > b)
+  d++;
+else 
+  break;
+}
+}
+
+int getNextPrime(int x){
+  int d,i,number;
+  int j= x+1;
+  for (d = x; d <= j; d++) { 
+    number = d;
+     for (i = 2; i < number; i++) {
+       if (number % i == 0) {
+         break;
+       }
+     }
+     if (i == number )
+       x=number;
+      else
+       j++;
+    }
+return x;
+}
+
+
+int getPrevPrime(int z){
+  int d,i,number,t;
+  for (d = 1; d < z; d++) { 
+    number = d;
+     for (i = 2; i < number; i++) {
+       if (number % i == 0) {
+         break;
+       }
+     }
+     if (i == number)
+       t=number;
+    }
+  z=t;
+return z;
+}
